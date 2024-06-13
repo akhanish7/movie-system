5b01cdc64c1db1489f933a3bbb057c1482f4172f
Hanish <akhanish7@gmail.com>
2024-06-14 03:37:27 +0530

    Added update and get movie api's

--- src/controller/movie.controller.js
+++ src/controller/movie.controller.js
@@ -7,8 +7,30 @@ const addMovie = async (req, res) => {
         await newMovie.save();
         res.status(201).json(newMovie);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+
     }
 };
 
-module.exports = {addMovie};
+const updateMovieDetails = async (req,res) => {
+    try{
+        const updateObj = req.body;
+        const movieId = req.params.id;
+        await MOVIE_MODEL.findOneAndUpdate({_id : movieId},{$set : updateObj}).exec();
+        res.status(200).json({msg : 'Movie Details Updated!'});
+    }catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
+const getAllMovies = async (req,res) => {
+    try{
+        const movieData = await MOVIE_MODEL.find().lean().exec();
+        res.status(200).json({data : movieData});
+    }catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
+module.exports = {addMovie,updateMovieDetails, getAllMovies};
